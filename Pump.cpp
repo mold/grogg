adaf7183ef440b666964e01bb085218e1e741575
Daniel Molin <dkd333@gmail.com>
2022-08-14 21:53:59 +0200

    Move Pump class back to Pump.cpp
    It works with include Arduino.h!

--- Pump.cpp
+++ Pump.cpp
@@ -1,1 +1,59 @@
+#include <Arduino.h>
 
+class Pump
+{
+private:
+    unsigned long startMillis;
+    float _ratio;
+    int timePeriodMillis = 2000;
+    int _pin;
+    int _speedPWM;
+
+    shouldPump()
+    {
+        unsigned long timeSinceStart = millis() - startMillis;
+        int timeElapsedInPeriod = timeSinceStart % timePeriodMillis;
+        float percentElapsedOfPeriod = (float)timeElapsedInPeriod / (float)timePeriodMillis;
+
+        // Serial.println("Should pump? timeSinceStart: " + String(timeSinceStart) + " timeElapsedInPeriod: " + String(timeElapsedInPeriod) + " percentElapsedOfPeriod: " + String(percentElapsedOfPeriod) + " ratio: " + String(_ratio));
+
+        return percentElapsedOfPeriod <= _ratio;
+    }
+
+public:
+    Pump() {}
+    Pump(int pin, float speedPWM)
+    {
+        _pin = pin;
+        _speedPWM = speedPWM;
+
+        pinMode(pin, OUTPUT);
+    }
+
+    start(float ratio)
+    {
+        startMillis = millis();
+        _ratio = ratio;
+
+        Serial.println("Start ratio: " + String(ratio, 2));
+    }
+
+    pump()
+    {
+        if (shouldPump())
+        {
+            analogWrite(_pin, _speedPWM);
+            // Serial.println("Should pump");
+        }
+        else
+        {
+            analogWrite(_pin, 0);
+            // Serial.println("Shout NOT pump");
+        }
+    }
+
+    stop()
+    {
+        analogWrite(_pin, 0);
+    }
+};
--- grogg.ino
+++ grogg.ino
@@ -1,55 +1,6 @@
 #include <LiquidCrystal.h>
 #include "Drinks.cpp"
-
-
-class Pump {
-  private:
-    unsigned long startMillis;
-    float _ratio;
-    int timePeriodMillis = 2000;
-    int _pin;
-    int _speedPWM;
-
-    shouldPump() {
-      unsigned long timeSinceStart = millis() - startMillis;
-      int timeElapsedInPeriod = timeSinceStart % timePeriodMillis;
-      float percentElapsedOfPeriod = (float)timeElapsedInPeriod / (float)timePeriodMillis;
-
-      // Serial.println("Should pump? timeSinceStart: " + String(timeSinceStart) + " timeElapsedInPeriod: " + String(timeElapsedInPeriod) + " percentElapsedOfPeriod: " + String(percentElapsedOfPeriod) + " ratio: " + String(_ratio));
-
-      return percentElapsedOfPeriod <= _ratio;
-
-    }
-  public:
-    Pump() {}
-    Pump(int pin, float speedPWM) {
-      _pin = pin;
-      _speedPWM = speedPWM;
-
-      pinMode(pin, OUTPUT);
-    }
-
-    start(float ratio) {
-      startMillis = millis();
-      _ratio = ratio;
-
-      Serial.println("Start ratio: " + String(ratio, 2));
-    }
-
-    pump() {
-      if (shouldPump()) {
-        analogWrite(_pin, _speedPWM);
-       // Serial.println("Should pump");
-      } else {
-        analogWrite(_pin, 0);
-       // Serial.println("Shout NOT pump");
-      }
-    }
-
-    stop() {
-      analogWrite(_pin, 0);
-    }
-};
+#include "Pump.cpp"
 
 LiquidCrystal lcd(13, 12, 4, 5, 6, 7);
 
